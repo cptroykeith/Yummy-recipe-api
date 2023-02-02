d93ca7d570699b5b6e123834f4e71545cf0b4b39
your-name <katongole.roy100@gmail.com>
2023-02-02 18:18:11 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-app.config.from_file('config')
+app.config.from_file('config.cfg', silent=True)
 
 db=SQLAlchemy(app)
 db.create_all()
