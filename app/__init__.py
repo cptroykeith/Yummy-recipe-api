d9251bb57821013bd5b8c9194a1da2d7beb8f9bf
your-name <katongole.roy100@gmail.com>
2023-02-07 12:03:22 +0300

    [bg] fix documentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,37 @@
-from flask import Flask
+from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
-app.config.from_file('config.cfg')
-db=SQLAlchemy(app)
-db.create_all()
+# local import
+from instance.config import app_config
+from flask_cors import CORS
 
-from app import views
+# initialize sql-alchemy
+db = SQLAlchemy()
+
+def create_app(config_name):
+    from app.models.category import Category
+    from app.models.recipe import Recipe
+    from app.models.recipeAuth import RecipeApp
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name]) #app.config.from_object(app_config['development'])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    CORS(app)
+    with app.app_context():
+        db.create_all()
+
+    # Blueprints
+    # import the authentication blueprint and register it on the app
+    from .auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # import the category blueprint and register it on the app
+    from .categories import category_api
+    app.register_blueprint(category_api)
+
+    # import the recipe blueprint and register it on the app
+    from .recipes import recipe_api
+    app.register_blueprint(recipe_api)
+
+    return app
