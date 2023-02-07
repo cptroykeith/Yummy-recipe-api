c53c77aa88349554e0bc5c6c0aae323061bd348d
your-name <katongole.roy100@gmail.com>
2023-02-07 13:03:21 +0300

    fixed config file

---
+++ instance/config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    SECRET = "abcdef"
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/recipe_db')
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI =  os.getenv('DATABASE_URL', 'postgresql://localhost/tests_db')
+    DEBUG = True
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
